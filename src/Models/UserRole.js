4a68bc348210a031d2b063f0f6e2d4d02ac52299
Ositadinma Nwangwu <osita.nwangwu@tenece.com>
2018-09-20 16:36:21 +0100

    created user role schema

---
+++ src/Models/UserRole.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+var UserRoleSchema = mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: [true,'Sorry, the user is required']
+    },
+    role: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: [true,'Sorry, the role is required']
+    }
+})
+
+module.exports = mongoose.model('UserRole',UserRoleSchema);
