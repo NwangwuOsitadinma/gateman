684790572637757bb6fedaaecc1bf91e22d9aa8e
Ositadinma Nwangwu <osita.nwangwu@tenece.com>
2018-09-20 16:00:57 +0100

    added validation messages for both fields

--- src/Models/RoleClaim.js
+++ src/Models/RoleClaim.js
@@ -3,11 +3,11 @@ const mongoose = require('mongoose');
 var RoleClaimSchema = mongoose.Schema({
     role: {
         type: mongoose.Schema.Types.ObjectId,
-        required: true
+        required: [true,'Sorry, the role is required']
     },
     claim: {
         type: mongoose.Schema.Types.ObjectId,
-        required: true
+        required: [true,'Sorry! the claim is required']
     }
 })
 
