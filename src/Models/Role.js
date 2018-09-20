93c94915c793750bf783ec634e3a64bfe1ec2977
Ositadinma Nwangwu <osita.nwangwu@tenece.com>
2018-09-20 16:19:56 +0100

    unique is not a validator

--- src/Models/Role.js
+++ src/Models/Role.js
@@ -4,7 +4,7 @@ var RoleSchema = mongoose.Schema({
     name: {
         type: String,
         required: [true,'Sorry, the role name is required'],
-        unique: [true,'Sorry, the role already exists']
+        unique: true
     }
 })
 
