354f519b491ae583eb7988b876d2663c3dcaf38b
Ositadinma Nwangwu <osita.nwangwu@tenece.com>
2018-09-21 10:07:08 +0100

    complete to function for my mind

--- src/HasRolesAndAbilities.js
+++ src/HasRolesAndAbilities.js
@@ -1,5 +1,24 @@
+const claim = require('./Models/Claim');
+const userClaim = require('./Models/UserClaim');
+
 class HasRolesAndAbilities{
-    constructor(){
+
+   to(claimName){
+       var c = claim.where('name',claimName);
+       if(c !== null){
+           userClaim.create({user: this._id,claim: c._id},function(err,usrClaim){
+               if(err) throw err;
+           })
+       }else{
+           claim.create({name:claimName},(err,clm) =>{
+                if(err) throw err;
+                userClaim.create({user: this._id,claim: clm._id},function(err,usrClaim){
+                    if(err) throw err;
+                })
+           })
+       }
+    }
+    from(){
         
     }
 }
