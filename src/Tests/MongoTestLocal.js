f335f15315483ba4cfa2ab1d0ae5409b7e276758
ibesoft11 <ibesoft11@gmail.com>
2018-09-25 10:43:29 +0100

    test function-chaining with new user model

--- src/Tests/MongoTestLocal.js
+++ src/Tests/MongoTestLocal.js
@@ -1,4 +1,5 @@
 var GateMan = require('../GateMan');
+var rolesAndAbilities = require('../HasRolesAndAbilities');
 var mongoose = require('mongoose');
 var express = require('express');
 
@@ -13,14 +14,35 @@ app.listen(3000, function(err){
     console.log("Server started successfully...");
 });
 
-app.get('/newClaim', newClaim);
+var UserModel = new mongoose.Schema({
+    name: String
+});
+UserModel.loadClass(rolesAndAbilities);
+var User = mongoose.model('User', UserModel);
 
 function newClaim(req, res){
     myGateMan.createClaim(req.query.name, function(err, claim){
         if (err) { 
             res.json({err: err});
         } else {
-            res.json({message: "Claim created successfully"});
+            res.json({message: "Claim created successfully", claim: claim});
         }
     });
 }
+function newRole(req, res){
+    myGateMan.createRole(req.query.name, function(err, role){
+        if (err) {
+            res.json({err: err});
+        } else {
+            res.json({message: 'Role created successfully', role: role});
+        }
+    });
+}
+
+app.get('/newClaim', newClaim);
+app.get('/newRole', newRole);
+app.get('/assign/:name', (req, res)=>{
+    User.create({name: req.params.name}, function(err, user){
+        res.json(200, myGateMan.allow(user).to('edit'));
+    });
+});
