d3cfc5b2380443b3f00d88d2e6ed9c372eaeb3b8
ibesoft11 <ibesoft11@gmail.com>
2018-09-25 10:44:31 +0100

    limit returned results to  one for now

--- src/HasRolesAndAbilities.js
+++ src/HasRolesAndAbilities.js
@@ -1,24 +1,28 @@
 const claim = require('./Models/Claim');
 const userClaim = require('./Models/UserClaim');
 
-class HasRolesAndAbilities{
-
+class HasRolesAndAbilities {
    to(claimName){
-       var c = claim.where('name',claimName);
-       if(c !== null){
-           userClaim.create({user: this._id,claim: c._id},function(err,usrClaim){
-               if(err) throw err;
-           })
-       }else{
-           claim.create({name:claimName},(err,clm) =>{
+       claim.where('name', claimName).limit(1).exec((err, c)=>{
+        if(c.length>0){
+            userClaim.create({user: this._id, claim: c[0]._id},function(err,usrClaim){
                 if(err) throw err;
-                userClaim.create({user: this._id,claim: clm._id},function(err,usrClaim){
-                    if(err) throw err;
-                })
-           })
-       }
+                return usrClaim;
+            })
+        }else{
+            claim.create({name:claimName},(err,clm) =>{
+                 if(err) throw err;
+                 userClaim.create({user: this._id,claim: clm._id},function(err,usrClaim){
+                     if(err) throw err;
+                     return usrClaim;
+                 })
+            })
+        }
+       });
     }
     from(){
         
     }
 }
+
+module.exports = HasRolesAndAbilities;
