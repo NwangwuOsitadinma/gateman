4f004b81ec7c8fe71550d29527e73a36eeb6fa4d
ibesoft11 <ibesoft11@gmail.com>
2018-11-06 07:01:31 +0100

    modified mongoose where clause

--- src/Models/Role.js
+++ src/Models/Role.js
@@ -2,8 +2,9 @@ const mongoose = require('mongoose');
 const roleClaim = require('./RoleClaim');
 const userRole = require('./UserRole');
 const claim = require('./Claim');
+var rolesAndAbilities = require('../HasRolesAndAbilities');
 
-var RoleSchema = mongoose.Schema({
+var RoleSchema = new mongoose.Schema({
     name: {
         type: String,
         required: [true,'Sorry, the role name is required'],
@@ -11,30 +12,41 @@ var RoleSchema = mongoose.Schema({
     }
 });
 
-RoleSchema.methods.to = (claimOrUser) =>{
-    if(typeof claimOrUser === String){
-        var c = claim.where('name',claimOrUser);
-        if(c !== null){
-            roleClaim.create({role:this._id,claim:c._id},function(err,roleClaim){
-                if(err) throw err;
-            })
-        }else{
-            claim.create({name:claimOrUser},(err,claimE) => {
-                if(err) throw err;
-                roleClaim.create({role:this._id,claim:claimE._id},function(err,roleClaim){
-                    if(err) throw err;
-                })
-            })
+class RoleMethods{
+    to (claimOrUser){
+        console.log("Role TO was called")
+        if (claimOrUser == null){
+             return console.log("I stopped because I was null")
         }
-    }else{
-        userRole.create({user:claimOrUser._id,role: this._id}, function(err,usrRole){
-            if(err) throw err;
-        })
+        if(typeof(claimOrUser) === "string"){
+          claim.where('name',claimOrUser).limit(1).exec((err, c)=>{
+            if(c.length > 0){
+                roleClaim.create({role:this._id,claim:c[0]._id},function(err,roleClaim){
+                    if(err) throw err;
+                    return roleClaim;
+                });
+            }else{
+                claim.create({name:claimOrUser},(err,claimE) => {
+                    if(err) throw err;
+                    roleClaim.create({role:this._id,claim:claimE._id},function(err,roleClaim){
+                        if(err) throw err;
+                        return roleClaim;
+                    });
+                });
+            }
+        });
+        } else{
+            userRole.create({user:claimOrUser._id,role: this._id}, function(err,usrRole){
+                if(err) throw err;
+                return usrRole;
+            })
+        } 
+}
+    modelName(){
+        return 'Role'
     }
+}
 
-}
-RoleSchema.methods.from = () => {
-    
-}
+RoleSchema.loadClass(RoleMethods)
 
 module.exports = mongoose.model('Role',RoleSchema); 
