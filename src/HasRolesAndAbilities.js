eb7994ecc776281f979526f8cab160a24fa8df70
ibesoft11 <ibesoft11@gmail.com>
2018-11-06 07:01:02 +0100

    modelName() shoud tell us the name of the mongoose model, temporary function

--- src/HasRolesAndAbilities.js
+++ src/HasRolesAndAbilities.js
@@ -3,12 +3,13 @@ const userClaim = require('./Models/UserClaim');
 
 class HasRolesAndAbilities {
    to(claimName){
+       console.log("HasRolesAndAbilities")
        claim.where('name', claimName).limit(1).exec((err, c)=>{
         if(c.length>0){
             userClaim.create({user: this._id, claim: c[0]._id},function(err,usrClaim){
                 if(err) throw err;
                 return usrClaim;
-            })
+            });
         }else{
             claim.create({name:claimName},(err,clm) =>{
                  if(err) throw err;
@@ -19,6 +20,9 @@ class HasRolesAndAbilities {
             })
         }
        });
+    }
+    modelName(){
+        return 'User'
     }
     from(){
         
