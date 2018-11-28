ab2d4c0230aa252601d2002fc17cc250d08ca352
Ositadinma <ossyemmanuel@gmail.com>
2018-11-28 09:39:26 +0100

    wrote a host of functions here. check out the description of each method for info

--- src/GateMan.js
+++ src/GateMan.js
@@ -7,52 +7,58 @@ const hasRoleAndAbilities = require('./HasRolesAndAbilities');
 var mongo = require('mongoose')
 
 class GateMan {
-
+    /**
+     * @param {A string that represents the name of the claim you want to create} claimName
+     * @param {A callback function that runs after the claim has been created} cb
+     */
     createClaim(claimName,cb){
         claim.create({name: claimName},cb);
     }
 
+    /**
+     * @param {A string that representst the name of the claim you want to create} roleName
+     * @param {A callback function that runs after the claim has been created} cb
+     */
     createRole(roleName,cb){
         role.create({name: roleName},cb);
     }
 
-    assign(roleName){
-      var r = role.where('name',roleName);
-      if(r !== null){
-          return r;
-      }
-      role.create({name: roleName},function(err,role){
-          if(err) throw err;
-          return role;
-      });
+    /**
+     * Assigns a role to a user
+     * @param {A mongoose object} role 
+     */
+    assign(role){
+        return role;
     }
 
-    retract(roleName){
-       
+    /**
+     * Retracts a role from a user
+     * @param {A mongoose object} role 
+     */
+    retract(role){
+       return role;
     }
 
     /**
      * Allows members of a role or a user to perform a claim
-     * @param {A mongoose object} roleNameorUser
+     * @param {A mongoose object} roleOrUser
      */
-    allow(roleNameorUser){
+    allow(roleOrUser){
         /*
          * Once we return the roleNameorUser object, mongoose automatically selects it's base model and calls it's respective to() function.
          */
-        console.log(roleNameorUser.modelName())//this should print the Model Name of its caller
+        console.log(roleOrUser.modelName())//this should print the Model Name of its caller
         console.log(role.modelName)//this should print Role
         return roleNameorUser;
     }
 
-    dissallow(roleName){
-        var r = role.where('name',roleName);
-        return r;
+    /**
+     * Dissallows a member of a role or user from performing a claim
+     * @param {A mongoose object} roleOrUser 
+     */
+    dissallow(roleOrUser){
+        return roleOrUser
     }
-
-    // to(claimName){
-    //     const rolesAndAbilities = new hasRoleAndAbilities(this);
-    //     return rolesAndAbilities.to(claimName);
-    // }
 
 }
 
--- src/HasRolesAndAbilities.js
+++ src/HasRolesAndAbilities.js
@@ -1,7 +1,17 @@
 const claim = require('./Models/Claim');
 const userClaim = require('./Models/UserClaim');
+const role = require('./Models/Role');
+const userRole =  require('./Models/UserRole');
 
 class HasRolesAndAbilities {
+    modelName(){
+        return 'User'
+    }
+
+   /**
+    * accepts a claim name and assigns it to the user
+    * @param {A string that represents the claim to be assigned} claimName 
+    */ 
    to(claimName){
        console.log("HasRolesAndAbilities")
        claim.where('name', claimName).limit(1).exec((err, c)=>{
@@ -21,9 +31,75 @@ class HasRolesAndAbilities {
         }
        });
     }
-    modelName(){
-        return 'User'
+
+    /**
+     * assigns the given claim to the user on which it is called
+     * @param {A mongoose object that represents the claim  you want to assign to a user} claim 
+     */
+    allow(claim){
+        userClaim.findOne({user: this._id,claim:claim._id},(err,uc) => {
+            if(uc.length>0){
+                return {message: 'this claim has already been assigned to this user'};
+            } else{
+                userClaim.create({user:this._id,claim:uc._id},(err,usrClaim) => {
+                    if(err) throw err;
+                    return usrClaim
+                })
+            }
+        })
     }
+
+    /**
+     * disallows a user from performing a particular claim
+     * @param {A mongoose object that represents the claim  you want to assign to a user} claim 
+     */
+    disallow(claim){
+        userClaim.findOne({user: this._id,claim:claim._id},(err,uc) => {
+            if(uc.length>0){
+                userClaim.deleteOne({user:this._id,claim:uc._id},(err) =>{
+                    if(err) throw err;
+                    return {message: 'the claim has been removed from the user'}
+                })
+            } else{
+                return{message: 'invalid action'}
+            }
+        })
+    }
+
+    /**
+     * assigns a role to a user directly
+     * @param {A mongoose object that represents the role you want to assign to a user} role 
+     */
+    assign(role){
+        userRole.findOne({user: this._id,role:role._id},(err,r)=>{
+            if(r.length>0){
+                return {message: 'this role has already been assigned to the user'};
+            } else{
+                userRole.create({user:this._id,role:r._id},function(err,ur){
+                    if(err) throw err;
+                    return ur;
+                })
+            }
+        })
+    }
+
+    /**
+     * retracts a role from a user directly
+     * @param {A mongoose model that represents the role you want to retract from the user} role 
+     */
+    retract(role){
+        userRole.findOne({user: this._id,claim:role._id},(err,ur) => {
+            if(ur.length>0){
+                userRole.deleteOne({user:this._id,claim:ur._id},(err) =>{
+                    if(err) throw err;
+                    return {message: 'the claim has been removed from the user'}
+                })
+            } else{
+                return{message: 'invalid action'}
+            }
+        })
+    }
+
     from(claimname){
         console.log("HasRolesAndAbilities");
         claim.where('name',claimname).limit(1).exec((err,c)=>{
