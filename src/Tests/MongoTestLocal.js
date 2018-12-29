5ff104962dcef340b85166fcde26c5112459de08
ibesoft11 <ibesoft11@gmail.com>
2018-12-29 20:37:52 +0100

    test new functions and re-design

--- src/Tests/MongoTestLocal.js
+++ src/Tests/MongoTestLocal.js
@@ -2,7 +2,7 @@ var GateMan = require('../GateMan');
 var rolesAndAbilities = require('../HasRolesAndAbilities');
 var mongoose = require('mongoose');
 var express = require('express');
-var role = require('../Models/Role')
+var role = require('../Models/Role');
 var app = express();
 
 mongoose.Promise = global.Promise;
@@ -14,14 +14,29 @@ app.listen(3000, function(err){
     console.log("Server started successfully...");
 });
 
-// var UserModel = new mongoose.Schema({
-//     name: String
-// });
+var UserModel = new mongoose.Schema({
+    name: String,
+    phone: Number
+});
 // UserModel.loadClass(rolesAndAbilities);
-// var User = mongoose.model('User', UserModel);
+//var User = mongoose.model('User', UserModel);
 
 app.get('/allowRole/:role', (req, res)=>{
     myGateMan.Roles.createRole(req.params.role, (err, role)=>{
-        res.json(myGateMan.Roles.allow(role.name).to('kill'));
+        res.json(myGateMan.allow(role.name).to('kill'));
+    });
+});
+app.get('/dissallow/:role/:claim',(req, res)=>{
+    res.json(myGateMan.Roles.dissallow(req.params.role).from(req.params.claim));
+});
+app.get('/roles', (req, res)=>{
+    myGateMan.getRoles((err, data)=>{
+        res.json(data);
+    });
+});
+app.get('/c', (req, res)=>{
+
+    myGateMan.getRoleClaims('accountant', (err, data)=>{
+        res.json(data);
     });
 });
