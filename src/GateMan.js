50c0feefff7a42eae66ddcb3aecc8a106571bb16
ibesoft11 <ibesoft11@gmail.com>
2018-12-29 08:03:07 +0100

    Changed variable naming

--- src/GateMan.js
+++ src/GateMan.js
@@ -4,18 +4,26 @@ const roleClaim = require('./Models/RoleClaim');
 const userClaim = require('./Models/UserClaim');
 const userRole = require('./Models/UserRole');
 const hasRoleAndAbilities = require('./HasRolesAndAbilities');
-const mongo = require('mongoose');
+const mongoose = require('mongoose');
 const roleClass = require('./Classes/Roles')
 
 class GateMan {
     //New Design begins here
     //Usage: myGateMan.Roles.allow('role').to('claim');
     //       myGateMan.Roles.assign('claim').to('role');
+    //       myGateMan.extend(UserModel);
+    //       UserModel.allow('user').to('claim');
     //       myGateMan.Users.allow('role').to('claim');
     //  	 myGateMan.Users.User('ibesoft').can('edit');
     //       myGateMan.Users.User('osita').isAn('admin');
     get Roles(){
         return new roleClass;
+    }
+    /**
+     * Provides a user model with the abilities of myGateMan
+     */
+    extend(UserModel){
+        return UserModel.loadClass(hasRoleAndAbilities); 
     }
 }
 
