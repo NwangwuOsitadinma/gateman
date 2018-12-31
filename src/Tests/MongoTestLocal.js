68b020a2d407d5d31a5e1a838f38f9b95a0aa0ee
ibesoft11 <ibesoft11@gmail.com>
2019-01-01 00:24:07 +0100

    test works well

--- src/Tests/MongoTestLocal.js
+++ src/Tests/MongoTestLocal.js
@@ -1,23 +1,24 @@
-var GateMan = require('../GateMan');
-var rolesAndAbilities = require('../HasRolesAndAbilities');
 var mongoose = require('mongoose');
-var express = require('express');
-var role = require('../Models/Role');
-var app = express();
 
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/GateManTest');
 
-var myGateMan = new GateMan();
+var GateMan = require('../GateMan');
+var rolesAndAbilities = require('../HasRolesAndAbilities');
+var express = require('express');
+var role = require('../Models/Role');
+var app = express();
 
 app.listen(3000, function(err){
     console.log("Server started successfully...");
 });
 
+var myGateMan = new GateMan(mongoose);
 var UserModel = new mongoose.Schema({
     name: String,
     phone: Number
 });
+new rolesAndAbilities(mongoose);
 UserModel.loadClass(rolesAndAbilities);
 var User = mongoose.model('Person', UserModel);
 
@@ -33,6 +34,9 @@ app.get('/roles', (req, res)=>{
     myGateMan.getRoles((err, data)=>{
         res.json(data);
     });
+});
+app.get('/mon',(req, res)=>{
+   res.json(mongoose.modelNames());
 });
 app.get('/c/:claim', (req, res)=>{
     User.findOne({name: "ibe", phone: 090909}, async (err, user)=>{
