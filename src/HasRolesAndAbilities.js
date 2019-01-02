684ce7b30ecbaf89a97173530ffd8582115a059b
Ositadinma <ossyemmanuel@gmail.com>
2019-01-02 12:25:00 +0100

    new disallow uses promises

--- src/HasRolesAndAbilities.js
+++ src/HasRolesAndAbilities.js
@@ -17,7 +17,7 @@ class HasRolesAndAbilities {
 
     /**
      * allows a user perform a claim, does nothing if user already has the claim
-     * @param {A string that represents the claim  you want to assign to a user} claim 
+     * @param {A string that represents the claim  you want to assign to a user} claimName 
      */
 
   allow(claimName){
@@ -51,21 +51,32 @@ class HasRolesAndAbilities {
 
     /**
      * disallows a user from performing a particular claim
-     * @param {A string that represents the claim you want to retract from a user} claim 
+     * @param {A string that represents the claim you want to retract from a user} claimName 
      */
-    disallow(Claim){
-        claim.findOne({name: Claim}, (err, claim)=>{
-            if (claim) {
-                userClaim.findOne({user: this._id,claim:claim._id},(err,uc) => {
-                    if(uc){
-                        userClaim.deleteOne({_id:uc._id}, (err)=>{
-                            return {message: 'the claim has been removed from the user'};
-                        });
-                    } else{
-                        return{message: 'invalid action, user claim does not exist'}
-                    }
-                });
-            }
+    disallow(claimName){
+        return new Promise((resolve,reject)=>{
+            claim.findOne({name: claimName}, (err, claim)=>{
+                if(claim){
+                    userClaim.findOne({user:this._id,claim:claim._id}, (e,uc)=>{
+                        if(e){
+                            reject(e)
+                        }else if(uc){
+                            userClaim.delete({user:this._id,claim:uc._id},(err,usrClaim)=>{
+                                if(err) reject(err);
+                                resolve(usrClaim);
+                            })
+                        } else{
+                            reject({
+                                message: "this claim was not assigned to the user"
+                            });
+                        }
+                    });
+                }else{
+                    reject({
+                        message: "The claim does not exist. Consider creating it first"
+                    });
+                }
+            });
         });
     }
 
