a573df0c9118294091d2617b8ccfcf4b0a25bc48
Ositadinma <ossyemmanuel@gmail.com>
2019-01-06 18:13:50 +0100

    create claim test

--- src/Tests/Gateman.spec.js
+++ src/Tests/Gateman.spec.js
@@ -1,18 +1,40 @@
-var assert =  require('assert');
-var should = require('chai').should();
-var expect = require('chai').expect();
+var chai = require('chai');
+var should = chai.should();
 var mongoose = require('mongoose');
+mongoose.set('useCreateIndex', true);
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/GateManTest');
-var Gateman = require('../GateMan')(mongoose);
+var gateman = require('../GateMan');
+var GateMan = new gateman(mongoose);
 
 
 describe('Gateman', function(){
+    beforeEach('ensuring the db is fresh',function(done){
+        console.log('before');
+        mongoose.connect('mongodb://localhost:27017/GateManTest',{useNewUrlParser: true});
+        GateMan.removeClaim('turn-water-into-wine').then((d)=>{});
+        done()
+    });
+    afterEach('ensuring everything is cleaned up',function(done){
+        console.log('after');
+        mongoose.disconnect();
+        done();
+    });
+
     describe('createClaim', function(){
-        it ('should return a claim object if created successfully',function(done){
-            Gateman.createClaim('turn-water-into-wine',(claim)=>{
-                assert.equal(claim,!null);
-            })
-        })
-    })
-})
+        it ('should return a claim object if created successfully',(done)=>{
+            
+            var c = 'turn-water-into-wine';   
+            GateMan.createClaim(c)
+            .then((claim)=>{
+                console.log(claim)
+                should.equal(claim.name,c);
+                done();
+            });
+        }).timeout(10000);
+    });
+
+
+
+    
+
+});
